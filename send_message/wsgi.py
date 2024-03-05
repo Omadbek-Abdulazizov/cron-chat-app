7a6b1dd8c6079b5d3d87f5648976a5e0bb5b13bd
Omadbek-Abdulazizov <82579690+Omadbek-Abdulazizov@users.noreply.github.com>
2024-03-05 11:15:10 +0500

    Update wsgi.py
    wsgi update

--- send_message/wsgi.py
+++ send_message/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'send_message.settings')
 
 application = get_wsgi_application()
+
+app = application
